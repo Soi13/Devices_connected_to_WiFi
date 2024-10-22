d96724105db91579f87790161d6c85b27d8bb2f1
Oleg Skvortsov <highsoft.soi@gmail.com>
2024-10-22 14:07:50 -0700

    Add regex for extracting connected devices from HTML page.

--- connected_devices.pl
+++ connected_devices.pl
@@ -21,11 +21,16 @@ my %form_data = (
 #Here we send post request with credentials and then get the page which is contain list of connected and not connected devices to Xfinity router
 #Successful response always return code 302 Found.
 my $response = $ua->post($url, \%form_data);
-
 my $response_list = $ua->get('http://10.0.0.1/at_a_glance.jst');
  
 if ($response_list->is_success) {
-    print $response_list->decoded_content;
+    my @list_of_connected_devices;
+    $response_list->decoded_content =~ m/(<h2 id="conndev">Connected Devices.*?\n)([^\n]*)/gm;
+    my $connected_devices_html = $2;
+    while ($connected_devices_html =~ m/(readonlyLabel">)(.*?)(<\/span><\/div>)/gm) {
+        push(@list_of_connected_devices, $2);
+    }
+    print join(", ", @list_of_connected_devices);
 }
 else {
     die $response_list->status_line;
